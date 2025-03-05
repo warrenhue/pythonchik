eaf45a75302c8f6c699fb7a2e47b27e2dd0638b8
admin <a@a.a>
2025-03-05 15:41:22 +0300

    task2 peredelala
    Signed-off-by: admin <a@a.a>

--- kr/task2.py
+++ kr/task2.py
@@ -1,5 +1,7 @@
-import math
-informatics = int(input())
 biologists = int(input())
+programmers = int(input())
 
-print((informatics*biologists) // math.gcd(informatics, biologists))
+for cake_pieces in range(max(biologists, programmers), biologists*programmers+1):
+    if cake_pieces % biologists == 0 and cake_pieces % programmers == 0:
+        print(cake_pieces)
+        break
